1782e83bd166839b46399fca62b96b6f2068e5e2
Kaori Toyonaga <kaori_toyonaga@yahoo.co.jp>
2021-04-26 13:53:43 +0000

    houses DB create

---
+++ db/migrate/20210426134100_create_houses.rb
@@ -0,0 +1,13 @@
+class CreateHouses < ActiveRecord::Migration[5.2]
+  def change
+    create_table :houses do |t|
+      t.string :title
+      t.integer :rent_fee
+      t.text :address
+      t.integer :age
+      t.text :remark
+
+      t.timestamps
+    end
+  end
+end
